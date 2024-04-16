c22a1a557e0239fc06012d312c543f21035f493f
Goldkey <93011647+goldk3y@users.noreply.github.com>
2024-04-15 17:51:31 -0700

    Update doodlejump.js

--- doodlejump.js
+++ doodlejump.js
@@ -5,8 +5,8 @@ let boardHeight = 576;
 let context;
 
 //doodler
-let doodlerWidth = 55.96;
-let doodlerHeight = 80;
+let doodlerWidth = 44.77;
+let doodlerHeight = 64;
 let doodlerX = boardWidth/2 - doodlerWidth/2;
 let doodlerY = boardHeight*7/8 - doodlerHeight;
 let doodlerRightImg;
